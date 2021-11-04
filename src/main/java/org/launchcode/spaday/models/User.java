28279799614222e46c98c5845967e80ca486e156
Forrest Burrelsman <forrestburrelsman@gmail.com>
2021-11-04 15:18:33 -0500

    base studio requirements complete.

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,12 +1,23 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotEmpty
+    @Size(min=5, max=15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(min=6)
     private String password;
 
     public User() {
-
     }
 
     public User(String username, String email, String password) {
